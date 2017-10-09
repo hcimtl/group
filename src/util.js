4cd0e75efebbbc3ab7df9682b576e95b1a430646
Thomas <thomasguthruf@gmail.com>
2017-10-09 23:36:17 +0200

    hide lang param

--- src/util.js
+++ src/util.js
@@ -99,6 +99,8 @@ function setHashParams(param, values){
   const toAdd = {}
   toAdd[param] = values
   $.extend(params, toAdd)
+  if(window.frameElement) delete params.lang 
+
 
   const hashStrings = []
 
