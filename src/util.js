deda2ed3b6315dd64d2c9d256051082626bf58f8
Thomas Guthruf <thomasguthruf@gmail.com>
2017-10-30 11:51:08 +0100

    accentFold function added for better search results

--- src/util.js
+++ src/util.js
@@ -138,5 +138,9 @@ function ajax(url, success) {
   return xhr;
 }
 
+function accentFold(inStr) {
+  return inStr.replace(/([àáâãäå])|([ç])|([èéêë])|([ìíîï])|([ñ])|([òóôõöø])|([ß])|([ùúûü])|([ÿ])|([æ])/g, function(str,a,c,e,i,n,o,s,u,y,ae) { if(a) return 'a'; else if(c) return 'c'; else if(e) return 'e'; else if(i) return 'i'; else if(n) return 'n'; else if(o) return 'o'; else if(s) return 's'; else if(u) return 'u'; else if(y) return 'y'; else if(ae) return 'ae'; });
+}
 
-export { saveAs, sortLocale, intersect, getHashParams, setHashParams, ajax, extend }
+
+export { saveAs, sortLocale, intersect, getHashParams, setHashParams, ajax, extend, accentFold }
